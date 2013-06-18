f5f818bf78bc31b8d73f182fc13150ca1036e07a
Andrew Raines <andrew@raines.me>
2013-06-18 11:47:05 -0500

    Real shard info.

--- src/main/java/org/elasticsearch/rest/action/unix/RestUnixShardsAction.java
+++ src/main/java/org/elasticsearch/rest/action/unix/RestUnixShardsAction.java
@@ -1,6 +1,10 @@
 package org.elasticsearch.rest.action.unix;
 
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -8,6 +12,7 @@ import org.elasticsearch.rest.*;
 import org.elasticsearch.rest.action.support.RestXContentBuilder;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
 
@@ -24,16 +29,40 @@ public class RestUnixShardsAction extends BaseRestHandler {
 
     @Override
     public void handleRequest(final RestRequest request, final RestChannel channel) {
-        RestStatus status = RestStatus.OK;
-        try {
-            channel.sendResponse(new StringRestResponse(status, "shard1\nshard2"));
-        } catch (Throwable e) {
-            try {
-                channel.sendResponse(new XContentThrowableRestResponse(request, e));
+        ClusterStateRequest clusterStateRequest = new ClusterStateRequest();
+        clusterStateRequest.listenerThreaded(false);
+        clusterStateRequest.filterMetaData(true);
+        clusterStateRequest.local(true);
+        client.admin().cluster().state(clusterStateRequest, new ActionListener<ClusterStateResponse>() {
+            @Override
+            public void onResponse(ClusterStateResponse clusterStateResponse) {
+                RestStatus status = RestStatus.OK;
+                try {
+                    StringBuilder out = new StringBuilder();
+                    for (Iterator<ShardRouting> it = clusterStateResponse.getState().getRoutingTable().allShards().iterator(); it.hasNext(); ) {
+                        ShardRouting shard = it.next();
+                        out.append(shard.index());
+                        out.append(" ");
+                        out.append(shard.id());
+                        out.append(" ");
+                        out.append(shard.primary());
+                        out.append("\n");
+                    }
+                    channel.sendResponse(new StringRestResponse(status, out.toString().trim()));
+                } catch (Throwable e) {
+                    try {
+                        channel.sendResponse(new XContentThrowableRestResponse(request, e));
 
-            } catch (Exception e1) {
-                logger.warn("Failed to send response", e);
+                    } catch (Exception e1) {
+                        logger.warn("Failed to send response", e);
+                    }
+                }
             }
-        }
+
+            @Override
+            public void onFailure(Throwable e) {
+                logger.error("wtf", e);
+            }
+        });
     }
 }
